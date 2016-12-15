6ce1f2f3d9c0a5f0551d33650838a1b7b4e9a6e6
simplon <aledorianlegrand@gmail.com>
2016-12-15 01:37:24 +0100

    done 2.0

--- js/Bootstrap-Portfolio.js
+++ js/Bootstrap-Portfolio.js
@@ -1,22 +1,64 @@
 $(function() {
 
-            $("#showMe").click(function() {
+    $("#showMe").click(function() {
 
-                var opa = $("#progress").css("opacity");
+        var opa = $("#progress").css("opacity");
 
-                opa = (opa == 1) ? 0 : 1;
+        opa = (opa == 1) ? 0 : 1;
 
-                $("#progress").animate({
-                    "opacity": opa
-                }, "slow");
-            });
+        $("#progress").animate({
+            "opacity": opa
+        }, "slow");
+    });
 
-            $('#Light').click(function(){
-              $('#lightT').attr("href", './css/Bootstrap-Portfolio-Light.css');
-            });
+    $('#Light').click(function() {
+        $('#lightT').attr("href", './css/Bootstrap-Portfolio-Light.css');
+    });
 
-            $('#Dark').click(function(){
-              $('#lightT').attr("href", './css/Bootstrap-Portfolio-Dark.css');
-            });
+    $('#Dark').click(function() {
+        $('#lightT').attr("href", './css/Bootstrap-Portfolio-Dark.css');
+    });
 
+    $('#formBTN').click(function() {
+        $('.fa-paper-plane').css('color', '#00611C');
+    });
+
+    // scroll handler
+  var scrollToAnchor = function( id ) {
+
+    // grab the element to scroll to based on the name
+    var elem = $("a[name='"+ id +"']");
+
+    // if that didn't work, look for an element with our ID
+    if ( typeof( elem.offset() ) === "undefined" ) {
+      elem = $("#"+id);
+    }
+
+    // if the destination element exists
+    if ( typeof( elem.offset() ) !== "undefined" ) {
+
+      // do the scroll
+      $('html, body').animate({
+              scrollTop: elem.offset().top
+      }, 1000 );
+
+    }
+  };
+
+  // bind to click event
+  $("a").click(function( event ) {
+
+    // only do this if it's an anchor link
+    if ( $(this).attr("href").match("#") ) {
+
+      // cancel default event propagation
+      event.preventDefault();
+
+      // scroll to the location
+      var href = $(this).attr('href').replace('#', '')
+      scrollToAnchor( href );
+
+    }
+
+  });
 });
